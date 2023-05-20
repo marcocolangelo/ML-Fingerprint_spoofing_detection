942deb43ee4667ff423e68141cf42638401e55ed
federicamato00 <134133171+federicamato00@users.noreply.github.com>
2023-05-20 11:35:31 +0200

    aggiornamenti load e main

--- load.py
+++ load.py
@@ -5,16 +5,27 @@ import scipy.special
 import itertools
 
 
-def load(path):
-    f=open(path,'r')
-    labels=[]
-    features=[]
-    for line in f:
+def load(pathT,pathE):
+    fT=open(pathT,'r')
+    fE=open(pathE,'r')
+    DTR=[]
+    DTE=[]
+    LTE=[]
+    LTR=[]
+    for line in fT:
         splitted=line.split(',')
-        features.append([float(i) for i in splitted[:-1]])
-        labels.append(int(splitted[-1]))
-    labels=np.array(labels)
-    features=np.array(features)
-    return features.T, labels 
+        DTR.append([float(i) for i in splitted[:-1]])
+        LTR.append(int(splitted[-1]))
+    DTR=np.array(DTR)
+    LTR=np.array(LTR)
+    for line in fE:
+        splitted=line.split(',')
+        DTE.append([float(i) for i in splitted[:-1]])
+        LTE.append(int(splitted[-1]))
+    DTE=np.array(DTE)
+    LTE=np.array(LTE)
+    fT.close()
+    fE.close()
+    return (DTR.T, LTR),(DTE.T,LTE)
         
 
